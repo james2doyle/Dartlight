9caa6f02d08fe986aedb47044e702df4c3fcd1d0
James Doyle <james2doyle@gmail.com>
2023-02-14 09:43:52 -0800

    Changed: rename project and prepare for package control

--- README.md
+++ README.md
@@ -1,37 +1,41 @@
-# Dartlight
-It's for now, a fork of the official Dart highlight JSON to work with Sublime Text and Textastic. Some minor changes were made to work as a tmLanguage file.
+# Flutter-Dart-Code
 
-## Installation for Sublime Text
+> A collection snippets, syntax support, and hot reload/restart functionality for Dart and Flutter code
 
-Once you have [Package Control](http://wbond.net/sublime_packages/package_control) installed, follow
-these instructions to install Dartlight for Sublime Text:
+Started as a fork of [elMuso/Dartlight](https://github.com/elMuso/Dartlight) but has grown into a more complete Dart and Flutter package.
 
-- Open the command palette:
+### Features
 
-  Operating System  | Keyboard Shortcut
-  ----------------  | -----------------
-  Linux             | <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>
-  Mac OS X          | <kbd>⌘</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd> 
-  Windows           | <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>P</kbd>
+*How is this different from [elMuso/Dartlight](https://github.com/elMuso/Dartlight)?*
 
-- Type 'install'
-- Select **Package Control: Install Package**
-- Type 'dartlight'
-- Select **Dartlight**
+- Uses native Sublime syntax highlighting, not Textmate bundle conversion
+- Includes support for **Hot Reload** and **Hot Restart** on file save
+- Better highlighting, overall
+- More completions with better defaults
+- Fold and indentation rules
 
-You may need to restart Sublime Text
-before you can start using all the features
-in the Dartlight plugin.
+### Installation
 
-## Installation for Textastic
+*With Git*
 
-On Mac OS X, clone this repository inside of `~/Library/Containers/com.textasticapp.textastic-mac/Data/Library/Application Support/Textastic`, then restart Textastic.
+- `cd $HOME/Library/Application\ Support/Sublime\ Text/Packages`
+- `git clone https://github.com/james2doyle/Flutter-Dart-Code`
 
-On iPad OS:
-- Install [Working Copy](https://apps.apple.com/us/app/working-copy/id896694807) from the App Store and use it to clone this repository
-- Start Textastic and go to 'Local Files'
-- Create a folder named `#Textastic` (case sensitive, it should get a special cogwheel icon)
-- Open both apps at once and drag the Dartlight folder into the `#Textastic` folder
-- Close Textastic completely by swiping upwards in the app switcher, then restart it.
+*With Package Control*
 
-Please note that Textastic does not update Dartlight automatically.
+- "Package Control > Add Repository"
+- https://github.com/james2doyle/Flutter-Dart-Code
+- "Package Control > Install Package"
+- Install "sublime-twig" package
+
+### TODOs
+
+- [x] hot restart/reload
+- [x] syntax
+- [x] comments
+- [x] folding
+- [x] indents
+- [x] dart 3.0 support (sealed, multiple returns)
+- [ ] improve logic for detecting running Flutter server
+- [ ] settings for toggling reload/restart
+- [ ] publish on Package Control
--- plugin.py
+++ plugin.py
@@ -6,29 +6,38 @@ import subprocess
 import platform
 
 
+# copied logic from https://github.com/reisub0/hot-reload.vim/blob/master/autoload/hotReload.vim#L5
 def run_reload():
     if platform.system() == "Darwin" or platform.system() == "Linux":
-        # send SIGUSR1 to trigger a hot reload and SIGUSR2 to trigger a hot restart
-        subprocess.check_call(
-            "kill -s USR1 \"$(pgrep -f flutter_tools.snapshot\\ run)\" &> /dev/null", shell=True)
+        try:
+            # send SIGUSR1 to trigger a hot reload
+            subprocess.call(
+                "kill -s USR1 \"$(pgrep -f flutter_tools.snapshot\\ run)\" &> /dev/null", shell=True)
+        except Exception as e:
+            sublime.error_message(
+                "FlutterDartCode: " + getattr(e, 'message', str(e)))
     else:
         # TODO support Windows
         sublime.error_message(
-            "Dartlight: hot reload is not supported on your platform")
+            "FlutterDartCode: hot reload is not supported on your platform")
 
 
 def run_restart():
     if platform.system() == "Darwin" or platform.system() == "Linux":
-        # send SIGUSR1 to trigger a hot reload and SIGUSR2 to trigger a hot restart
-        subprocess.check_call(
-            "kill -s USR2 \"$(pgrep -f flutter_tools.snapshot\\ run)\" &> /dev/null", shell=True)
+        try:
+            # send SIGUSR2 to trigger a hot restart
+            subprocess.call(
+                "kill -s USR2 \"$(pgrep -f flutter_tools.snapshot\\ run)\" &> /dev/null", shell=True)
+        except Exception as e:
+            sublime.error_message(
+                "FlutterDartCode: " + getattr(e, 'message', str(e)))
     else:
         # TODO support Windows
         sublime.error_message(
-            "Dartlight: hot reload is not supported on your platform")
+            "FlutterDartCode: hot reload is not supported on your platform")
 
 
-class Dartlight(sublime_plugin.EventListener):
+class FlutterDartCode(sublime_plugin.EventListener):
     def on_post_save_async(self, view):
         file_path = view.file_name()
         if not file_path:
@@ -45,10 +54,11 @@ class Dartlight(sublime_plugin.EventListener):
                 sublime.set_timeout(lambda: view.erase_status(
                     "flutter_reloading"), 1000)
             except Exception as e:
-                return sublime.error_message("Failed to trigger hot reload on save. " + getattr(e, 'message', str(e)))
+                return sublime.error_message(
+                    "Failed to trigger hot reload on save. " + getattr(e, 'message', str(e)))
 
 
-class DartlightHotReloadCommand(sublime_plugin.TextCommand):
+class FlutterDartCodeHotReloadCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.set_status("flutter_reloading", "Reloading...")
         sublime.set_timeout_async(run_reload)
@@ -56,7 +66,7 @@ class DartlightHotReloadCommand(sublime_plugin.TextCommand):
             "flutter_reloading"), 1000)
 
 
-class DartlightHotRestartCommand(sublime_plugin.TextCommand):
+class FlutterDartCodeHotRestartCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.set_status("flutter_restarting", "Restarting...")
         sublime.set_timeout_async(run_restart)
